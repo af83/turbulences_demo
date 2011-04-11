3c04dee28f27472151b0f55be5cf787aae9265da
François de Metz <fdemetz@af83.com>
2011-04-11 11:13:53 +0200

    Add request package as dependency.
    Signed-off-by: François de Metz <fdemetz@af83.com>

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require('express'),
-    oauth2_client = require('oauth2-client');
+var express = require('express')
+  , request = require('request')
+  , oauth2_client = require('oauth2-client');
 
 var base_url = 'http://127.0.0.1:3000';
 var config = {
@@ -19,8 +20,7 @@ var config = {
         server_token_endpoint: 'http://localhost:7070/oauth2/token',
 
         client_id: "4d9337046fbf419313000004", // TODO: define this before running
-        client_secret: 'some secret string',
-        name: 'test_client'
+        client_secret: 'some secret string'
       }
     }
   }
@@ -43,10 +43,6 @@ var oauth2_client_options = {
 };
 
 var client = oauth2_client.createClient(config.oauth2_client, oauth2_client_options);
-
-
-
-
 
 var server = express.createServer(
     express.static(__dirname + '/public'),
